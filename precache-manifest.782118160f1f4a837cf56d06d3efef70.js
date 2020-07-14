5273ccd43cbcf287ee8c36b9c9f65c88711b65c0
V <SV067337@cerner.net>
2020-07-14 11:59:53 +0530

    Updates

---
+++ precache-manifest.782118160f1f4a837cf56d06d3efef70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fc1b61b2bc866760f5159e7472f287",
+    "url": "/youtube-api/index.html"
+  },
+  {
+    "revision": "827b892c96e3fde75d72",
+    "url": "/youtube-api/static/css/main.04f3e38d.chunk.css"
+  },
+  {
+    "revision": "454d4e17da32a9807c91",
+    "url": "/youtube-api/static/js/2.5570f39d.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube-api/static/js/2.5570f39d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827b892c96e3fde75d72",
+    "url": "/youtube-api/static/js/main.192d8af1.chunk.js"
+  },
+  {
+    "revision": "10b14b9277a5c147a271",
+    "url": "/youtube-api/static/js/runtime-main.8700de62.js"
+  }
+]);
